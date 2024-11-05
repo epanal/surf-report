3ed43918a56ab606e653172d0eb388ce05ce4a0b
epanal <epanal@gmail.com>
2024-11-04 16:33:06 -0800

    automated daily database update

--- dags/daily_dag.py
+++ dags/daily_dag.py
@@ -7,6 +7,8 @@ import boto3
 import logging
 import pendulum
 import os
+import psycopg2
+
 
 # Set the timezone to GMT
 gmt_timezone = pendulum.timezone("GMT")
@@ -56,6 +58,100 @@ def load_s3_data():
         logging.error(f"Error uploading to S3: {str(e)}")
         raise  # This will help show the full error in Airflow logs
 
+# download and load the data into the SQL database
+def download_and_load_s3_data():
+
+    aws_access_key = Variable.get('AWS_ACCESS_KEY_ID')
+    aws_secret_key = Variable.get('AWS_SECRET_ACCESS_KEY')
+    # Data directory matches your docker-compose volume mapping
+    DATA_DIR = './processed_data/buoy'
+
+
+    #     # Establishing the connection
+    conn = psycopg2.connect(
+        database="airflow",
+        user="airflow",
+        password="airflow",
+        host="host.docker.internal",
+        port="5432"
+    )
+    cursor = conn.cursor()
+
+    # Executing a function using the execute() method
+    cursor.execute("SELECT version()")
+    data = cursor.fetchone()
+    print("Connection established to: ", data)
+
+    s3 = boto3.client('s3',
+                      aws_access_key_id=aws_access_key,
+                      aws_secret_access_key=aws_secret_key,)
+    
+    # Get latest file from S3
+    get_last_modified = lambda obj: int(obj['LastModified'].strftime('%s'))
+    objs = s3.list_objects_v2(Bucket='surflinehmbbucket')['Contents']
+    last_added = [obj['Key'] for obj in sorted(objs, key=get_last_modified)][-1]
+    print(last_added)
+    # Download file from S3 # Use basename for the local file path
+    local_file_path = os.path.join(DATA_DIR, os.path.basename(last_added))
+    print(local_file_path)
+    s3.download_file('surflinehmbbucket', last_added, local_file_path)
+
+    command = """
+            CREATE TABLE IF NOT EXISTS surf_report_aptos (
+                id SERIAL PRIMARY KEY,
+                report_timestamp TIMESTAMP,
+                swell_height_ft NUMERIC,
+                swell_period_sec NUMERIC,
+                air_temp_f NUMERIC,
+                water_temp_f NUMERIC
+            );
+            
+            CREATE TEMPORARY TABLE staging_surf_report (
+                report_date VARCHAR(255),
+                report_time VARCHAR(255),
+                swell_height_ft NUMERIC,
+                swell_period_sec NUMERIC,
+                air_temp_f NUMERIC,
+                water_temp_f NUMERIC
+            );
+        """
+    cursor.execute(command)
+    conn.commit()
+
+    # Load data from file
+    with open(local_file_path, 'r') as f:
+        # Skip header row
+        next(f)
+        try:
+            cursor.copy_from(f, 'staging_surf_report', sep=',', null='NA')
+            print("Data inserted into staging table successfully")
+        except (Exception, psycopg2.DatabaseError) as err:
+            print("Error loading data:", err)
+            raise
+
+    command = """
+        INSERT INTO surf_report_aptos 
+        (report_timestamp, swell_height_ft, swell_period_sec, air_temp_f, water_temp_f)
+        SELECT 
+            TO_TIMESTAMP(report_date || ' ' || report_time, 'MM/DD/YY HH24:MI GMT'),
+            swell_height_ft,
+            swell_period_sec,
+            air_temp_f,
+            water_temp_f
+        FROM staging_surf_report
+        WHERE NOT EXISTS (
+            SELECT 1
+            FROM surf_report_aptos
+            WHERE surf_report_aptos.report_timestamp = 
+                TO_TIMESTAMP(staging_surf_report.report_date || ' ' || 
+                            staging_surf_report.report_time, 'MM/DD/YY HH24:MI GMT')
+            );
+    """
+    cursor.execute(command)
+    conn.commit()
+    cursor.close()
+    conn.close()
+
 # Create the new DAG for daily execution
 with DAG(
     's3_data',
@@ -69,3 +165,10 @@ with DAG(
         task_id='load_data_to_s3',
         python_callable=load_s3_data
     )
+    download_s3_load_sql_task= PythonOperator(
+    task_id='download_and_load_s3_data',
+    python_callable=download_and_load_s3_data
+)
+
+# Setting task dependencies
+load_s3_task >> download_s3_load_sql_task
